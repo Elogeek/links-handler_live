dc4478d4b941b2cf1ab98ff1ed1fa508f4640a0f
elogeek <elodiechristin@gmail.com>
2021-10-28 10:06:56 +0200

    deployment

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
 </head>
 <body>
     version Dev/ php -S localhost:8000 ===> version préprod
-     Coucou coucou coucou tranquille
+     Coucou coucou coucou tranquille . Hello from dev branch !
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque delectus deserunt doloremque enim et expedita
+        iste, itaque laboriosam laborum molestias nam, possimus reiciendis repellendus reprehenderit sapiente sequi
+        vitae voluptatem voluptates!
+    </p>
 </body>
 </html>
